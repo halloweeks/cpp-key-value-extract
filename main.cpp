8e77d000341ae8bc99302da4d613eb49f5e846e2
Hallo Weeks <96823158+halloweeks@users.noreply.github.com>
2022-12-11 17:46:44 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,14 +1,34 @@
 #include <iostream>
+#include <vector>
+#include <sstream>
 
-int main() {
-	char str[] = "name=hello\r\npass=12345\r\nuser_id=55555\r\nhhfdjkkjbvg=jjjhhh\r\nbjjjggg=hhh\r\nhhhffbnn";
-	
-	bool START = true, END = false;
+class config {
+	private:
 	char KEY[1024] = {0};
 	char VALUE[1024] = {0};
+	bool START = true, END = false;
 	int index = 0;
-	
-	for (int i = 0; i < sizeof(str); i++) {
+	std::vector <std::string> keys, values;
+	std::string data;
+	public:
+	void parse(const char *);
+	bool isset(std::string);
+	config& value(std::string);
+	bool Bool();
+	int Int();
+	unsigned int Uint();
+	long Long();
+	unsigned long Ulong();
+	long long LongLong();
+	unsigned long long UlongLong();
+	float Float();
+	double Double();
+	long double LongDouble();
+	std::string String();
+};
+
+void config::parse(const char *str) {
+	for (long int i = 0; i < strlen(str)+1; i++) {
 		if (START) {
 			if (str[i] == '=') {
 				START = false;
@@ -36,15 +56,128 @@ int main() {
 				}
 			}
 			if (END) {
-				std::cout << "KEY: '" << KEY << "' VALUE: '" << VALUE << "'\n";
+				keys.push_back(KEY);
+				values.push_back(VALUE);
 				memset(KEY, 0, 1024);
 				memset(VALUE, 0, 1024);
 				START = true;
 				END = false;
-				index = 0;
 			}
 		}
 	}
+}
+
+bool config::isset(std::string key) {
+	for (unsigned int index = 0; index < keys.size(); index++) {
+		if (key == keys[index]) {
+			return true;
+		}
+	}
+	return false;
+}
+
+
+config& config::value(std::string key) {
+	for (unsigned int index = 0; index < keys.size(); index++) {
+		if (key == keys[index]) {
+			data = values[index];
+			break;
+		}
+	}
+	return *this;
+}
+
+
+bool config::Bool() {
+	if (data == "1" || data == "true") {
+		return true;
+	}
 	
+	if (data == "0" || data == "false") {
+		return false;
+	}
+	return false;
+}
+
+int config::Int() {
+	std::istringstream reader(data);
+    int val = 0;
+    reader >> val;
+    return val;
+}
+
+unsigned int config::Uint() {
+	std::istringstream reader(data);
+    unsigned int val = 0;
+    reader >> val;
+    return val;
+}
+
+long config::Long() {
+	std::istringstream reader(data);
+    long val = 0;
+    reader >> val;
+    return val;
+}
+
+unsigned long config::Ulong() {
+    std::istringstream reader(data);
+    unsigned int val = 0;
+    reader >> val;
+    return val;
+}
+
+long long config::LongLong() {
+	std::istringstream reader(data);
+    long long val = 0;
+    reader >> val;
+    return val;
+}
+
+unsigned long long config::UlongLong() {
+	std::istringstream reader(data);
+    unsigned long long val = 0;
+    reader >> val;
+    return val;
+}
+
+float config::Float() {
+	std::istringstream reader(data);
+    float val = 0.0;
+    reader >> val;
+    return val;
+}
+
+double config::Double() {
+	std::istringstream reader(data);
+    double val = 0.0;
+    reader >> val;
+    return val;
+}
+
+long double config::LongDouble() {
+	std::istringstream reader(data);
+    long double val = 0.00;
+    reader >> val;
+    return val;
+}
+
+std::string config::String() {
+	return data;
+}
+
+config config;
+
+int main() {
+	const char *str = "username=user123\r\nemail=email@email.com\r\npassword=1234";
+	
+	config.parse(str);
+	
+	std::cout << std::boolalpha;
+	std::cout << "key username exist: " << config.isset("username") << "\n";
+	std::cout << "value of username: " << config.value("username").String() << "\n";
+	
+	std::cout << "key email exist: " << config.isset("email") << "\n";
+	std::cout << "value of email: " << config.value("email").String() << "\n";
 	return 0;
 }
